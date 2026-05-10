98dc55c0f1ac517ba2d53d07e85fd245dfc81bf6
Thiago Severo <severo.thiago2006@gmail.com>
2026-05-10 16:22:23 -0300

    build: criando a estrutura de pastas incial

---
+++ src/main/java/app/GIF.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package app;
+
+/**
+ *
+ * @author sever
+ */
+public class GIF {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
