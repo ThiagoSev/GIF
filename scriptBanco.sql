60847b5bdc35cfb2c687c14cf551499e8d83b7ff
CaikeHenry007 <caikehenry12@gmail.com>
2026-05-18 20:23:13 -0300

    fix: correção de script do Banco de Dados

--- scriptBanco.sql
+++ scriptBanco.sql
@@ -1,100 +1,125 @@
---CREATE DATABASE gif;
+CREATE DATABASE gif;
 
---usuario
+-- usuario do sistema
 CREATE ROLE gif
 LOGIN
 PASSWORD 'gif';
 
 GRANT ALL PRIVILEGES
 ON ALL TABLES IN SCHEMA public
-TO sistema;
+TO gif;
 
 GRANT ALL PRIVILEGES
 ON ALL SEQUENCES IN SCHEMA public
-TO sistema;
+TO gif;
 
-
---tabelas
-
-DROP TABLE IF EXISTS usuario;
-DROP TABLE IF EXISTS distribuidor;
-DROP TABLE IF EXISTS genero;
-DROP TABLE IF EXISTS jogo;
-DROP TABLE IF EXISTS generoJogo;
-DROP TABLE IF EXISTS bibliotecajogos;
-DROP TABLE IF EXISTS carrinho;
+-- remover tabelas
 DROP TABLE IF EXISTS carrinhoitens;
+DROP TABLE IF EXISTS carrinho;
+DROP TABLE IF EXISTS bibliotecajogos;
+DROP TABLE IF EXISTS generoJogo;
+DROP TABLE IF EXISTS jogo;
+DROP TABLE IF EXISTS genero;
+DROP TABLE IF EXISTS distribuidor;
+DROP TABLE IF EXISTS usuario;
+
+-- tabelas
 
 CREATE TABLE usuario
 (
     id SERIAL PRIMARY KEY,
-    nome varchar(20) NOT NULL,
-    apelido varchar(20) NOT NULL,
-    senha varchar(20) NOT NULL,
-    datanascimento timestamp without time zone
+    nome VARCHAR(20) NOT NULL,
+    apelido VARCHAR(20) NOT NULL,
+    senha VARCHAR(20) NOT NULL,
+    datanascimento TIMESTAMP
 );
 
 CREATE TABLE distribuidor
 (
     id SERIAL PRIMARY KEY,
-    nome varchar(20) NOT NULL,
-    cnpj varchar(20) NOT NULL,
-    senha varchar(20) NOT NULL
+    nome VARCHAR(20) NOT NULL,
+    cnpj VARCHAR(20) NOT NULL,
+    senha VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE genero(
+CREATE TABLE genero
+(
     id SERIAL PRIMARY KEY,
-    descricao text DEFAULT ''
+    descricao TEXT DEFAULT ''
 );
 
 CREATE TABLE jogo
 (
     id SERIAL PRIMARY KEY,
-    titulo varchar(30) NOT NULL,
-    subtitulo varchar(50) NOT NULL,
-    descricao text DEFAULT '',
+    titulo VARCHAR(30) NOT NULL,
+    subtitulo VARCHAR(50) NOT NULL,
+    descricao TEXT DEFAULT '',
     precopadrao NUMERIC(6,2) NOT NULL,
-    preçopromocao NUMERIC(6,2),
-    estaempromocao BOOL DEFAULT false,
-    datalançamento timestamp without time zone,
-    iddistribuidor int,
-    idcriador int,
-    FOREIGN KEY (iddistribuidor) REFERENCES distribuidor ON DELETE CASCADE,
-    FOREIGN KEY (idcriador) REFERENCES usuario ON DELETE CASCADE
+    precopromocao NUMERIC(6,2),
+    estaempromocao BOOLEAN DEFAULT false,
+    datalancamento TIMESTAMP,
+    iddistribuidor INT,
+    idcriador INT,
+
+    FOREIGN KEY (iddistribuidor)
+    REFERENCES distribuidor(id)
+    ON DELETE CASCADE,
+
+    FOREIGN KEY (idcriador)
+    REFERENCES usuario(id)
+    ON DELETE CASCADE
 );
 
-CREATE TABLE generoJogo(
-    idgenero int,
-    idjogo int,
+CREATE TABLE generoJogo
+(
+    idgenero INT,
+    idjogo INT,
+
     PRIMARY KEY(idgenero, idjogo),
-    FOREIGN KEY (idgenero) REFERENCES genero ON DELETE CASCADE,
-    FOREIGN KEY (idjogo) REFERENCES jogo ON DELETE CASCADE
+
+    FOREIGN KEY (idgenero)
+    REFERENCES genero(id)
+    ON DELETE CASCADE,
+
+    FOREIGN KEY (idjogo)
+    REFERENCES jogo(id)
+    ON DELETE CASCADE
 );
 
-
-CREATE TABLE bibliotecajogos(
-    iddono int,
-    idjogo int,
-    dataaquisicao timestamp without time zone,
-    tempodejogo timestamp without time zone,
-    percentualconquistas numeric(3,2) DEFAULT 0
+CREATE TABLE bibliotecajogos
+(
+    iddono INT,
+    idjogo INT,
+    dataaquisicao TIMESTAMP,
+    tempodejogo TIMESTAMP,
+    percentualconquistas NUMERIC(3,2) DEFAULT 0
 );
 
-CREATE TABLE carrinho(
+CREATE TABLE carrinho
+(
     id SERIAL PRIMARY KEY,
-    idusuario int,
-    datacriacao timestamp without time zone,
-    ultimaatualizacao timestamp without time zone,
-    FOREIGN KEY (idusuario) REFERENCES jogo ON DELETE CASCADE
-    
+    idusuario INT,
+    datacriacao TIMESTAMP,
+    ultimaatualizacao TIMESTAMP,
+
+    FOREIGN KEY (idusuario)
+    REFERENCES usuario(id)
+    ON DELETE CASCADE
 );
 
-CREATE TABLE carrinhoitens(
-    idjogo int,
-    idcarrinho int,
-    quantidade int,
-    PRIMARY KEY(idjogo, idcarrinho),
-    FOREIGN KEY (idjogo) REFERENCES jogo ON DELETE CASCADE,
-    FOREIGN KEY (idcarrinho) REFERENCES carrinho ON DELETE CASCADE
+CREATE TABLE carrinhoitens
+(
+    idjogo INT,
+    idcarrinho INT,
+    quantidade INT,
 
+    PRIMARY KEY(idjogo, idcarrinho),
+
+    FOREIGN KEY (idjogo)
+    REFERENCES jogo(id)
+    ON DELETE CASCADE,
+
+    FOREIGN KEY (idcarrinho)
+    REFERENCES carrinho(id)
+    ON DELETE CASCADE
 );
