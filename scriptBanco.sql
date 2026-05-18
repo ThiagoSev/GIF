a8efeb2dea4f7435bbd4eb12ca99372af06895c6
CaikeHenry007 <caikehenry12@gmail.com>
2026-05-18 19:33:04 -0300

    feat: alteração de script do Banco de Dados

--- scriptBanco.sql
+++ scriptBanco.sql
@@ -1,7 +1,6 @@
-CREATE DATABASE gif
+--CREATE DATABASE gif;
 
 --usuario
-
 CREATE ROLE gif
 LOGIN
 PASSWORD 'gif';
@@ -17,9 +16,85 @@ TO sistema;
 
 --tabelas
 
-CREATE TABLE IF NOT EXISTS usuario
+DROP TABLE IF EXISTS usuario;
+DROP TABLE IF EXISTS distribuidor;
+DROP TABLE IF EXISTS genero;
+DROP TABLE IF EXISTS jogo;
+DROP TABLE IF EXISTS generoJogo;
+DROP TABLE IF EXISTS bibliotecajogos;
+DROP TABLE IF EXISTS carrinho;
+DROP TABLE IF EXISTS carrinhoitens;
+
+CREATE TABLE usuario
 (
     id SERIAL PRIMARY KEY,
-    nome character varchar(20) NOT NULL,
-    senha character varchar(20) NOT NULL
-)
+    nome varchar(20) NOT NULL,
+    apelido varchar(20) NOT NULL,
+    senha varchar(20) NOT NULL,
+    datanascimento timestamp without time zone
+);
+
+CREATE TABLE distribuidor
+(
+    id SERIAL PRIMARY KEY,
+    nome varchar(20) NOT NULL,
+    cnpj varchar(20) NOT NULL,
+    senha varchar(20) NOT NULL
+);
+
+CREATE TABLE genero(
+    id SERIAL PRIMARY KEY,
+    descricao text DEFAULT ''
+);
+
+CREATE TABLE jogo
+(
+    id SERIAL PRIMARY KEY,
+    titulo varchar(30) NOT NULL,
+    subtitulo varchar(50) NOT NULL,
+    descricao text DEFAULT '',
+    precopadrao NUMERIC(6,2) NOT NULL,
+    preçopromocao NUMERIC(6,2),
+    estaempromocao BOOL DEFAULT false,
+    datalançamento timestamp without time zone,
+    iddistribuidor int,
+    idcriador int,
+    FOREIGN KEY (iddistribuidor) REFERENCES distribuidor ON DELETE CASCADE,
+    FOREIGN KEY (idcriador) REFERENCES usuario ON DELETE CASCADE
+);
+
+CREATE TABLE generoJogo(
+    idgenero int,
+    idjogo int,
+    PRIMARY KEY(idgenero, idjogo),
+    FOREIGN KEY (idgenero) REFERENCES genero ON DELETE CASCADE,
+    FOREIGN KEY (idjogo) REFERENCES jogo ON DELETE CASCADE
+);
+
+
+CREATE TABLE bibliotecajogos(
+    iddono int,
+    idjogo int,
+    dataaquisicao timestamp without time zone,
+    tempodejogo timestamp without time zone,
+    percentualconquistas numeric(3,2) DEFAULT 0
+);
+
+CREATE TABLE carrinho(
+    id SERIAL PRIMARY KEY,
+    idusuario int,
+    datacriacao timestamp without time zone,
+    ultimaatualizacao timestamp without time zone,
+    FOREIGN KEY (idusuario) REFERENCES jogo ON DELETE CASCADE
+    
+);
+
+CREATE TABLE carrinhoitens(
+    idjogo int,
+    idcarrinho int,
+    quantidade int,
+    PRIMARY KEY(idjogo, idcarrinho),
+    FOREIGN KEY (idjogo) REFERENCES jogo ON DELETE CASCADE,
+    FOREIGN KEY (idcarrinho) REFERENCES carrinho ON DELETE CASCADE
+
+);
