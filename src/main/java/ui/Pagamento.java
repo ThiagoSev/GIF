39a42d48b96beb6a8abf11570836c63c6b0e1769
Thiago Severo <severo.thiago2006@gmail.com>
2026-06-18 17:12:25 -0300

    feat: adicionando campos do cartão no pagamento

--- src/main/java/ui/Pagamento.java
+++ src/main/java/ui/Pagamento.java
@@ -11,6 +11,7 @@ public class Pagamento extends javax.swing.JFrame {
     
     public Pagamento(CarrinhoModel carrinho) {
         initComponents();
+        setLocationRelativeTo(null);
         this.carrinho = carrinho;
     }
 
@@ -22,12 +23,25 @@ public class Pagamento extends javax.swing.JFrame {
         jLTitulo = new javax.swing.JLabel();
         jComboBox1 = new javax.swing.JComboBox<>();
         BtnContinuar = new javax.swing.JButton();
+        numeroCartao = new javax.swing.JTextField();
+        nomeTitular = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        validadeCartao = new javax.swing.JTextField();
+        codigoSegurancaCartao = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jLTitulo.setText("Forma de Pagamento");
 
         jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Pix", "Cartão Mastercard" }));
+        jComboBox1.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                jComboBox1FocusLost(evt);
+            }
+        });
 
         BtnContinuar.setText("Continuar");
         BtnContinuar.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -36,6 +50,22 @@ public class Pagamento extends javax.swing.JFrame {
             }
         });
 
+        numeroCartao.setText("jTextField1");
+
+        nomeTitular.setText("jTextField1");
+
+        jLabel1.setText("Número do cartão");
+
+        jLabel2.setText("nome do titular");
+
+        jLabel3.setText("Validade");
+
+        validadeCartao.setText("jTextField1");
+
+        codigoSegurancaCartao.setText("jTextField1");
+
+        jLabel4.setText("CVV");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -43,10 +73,27 @@ public class Pagamento extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLTitulo)
-                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(BtnContinuar))
-                .addContainerGap(263, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(jLTitulo)
+                                .addComponent(jComboBox1, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(BtnContinuar)
+                                .addComponent(numeroCartao))
+                            .addComponent(jLabel1))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel2)
+                            .addComponent(nomeTitular, javax.swing.GroupLayout.PREFERRED_SIZE, 204, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(validadeCartao, javax.swing.GroupLayout.PREFERRED_SIZE, 83, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel3))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel4)
+                            .addComponent(codigoSegurancaCartao, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(47, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -55,7 +102,23 @@ public class Pagamento extends javax.swing.JFrame {
                 .addComponent(jLTitulo)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 191, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel2))
+                .addGap(1, 1, 1)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(numeroCartao, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(nomeTitular, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel4))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(validadeCartao, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(codigoSegurancaCartao, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 84, Short.MAX_VALUE)
                 .addComponent(BtnContinuar)
                 .addGap(16, 16, 16))
         );
@@ -78,6 +141,21 @@ public class Pagamento extends javax.swing.JFrame {
         CarrinhoService service = new CarrinhoService();
         service.ComprarCarrinho(carrinho);
     }//GEN-LAST:event_BtnContinuarMouseClicked
+
+    private void jComboBox1FocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_jComboBox1FocusLost
+       if(String.valueOf(jComboBox1.getSelectedItem()) == "Pix"){
+           nomeTitular.setVisible(false);
+           numeroCartao.setVisible(false);
+           validadeCartao.setVisible(false);
+           codigoSegurancaCartao.setVisible(false);
+       }
+       else{
+           nomeTitular.setVisible(true);
+           numeroCartao.setVisible(true);
+           validadeCartao.setVisible(true);
+           codigoSegurancaCartao.setVisible(true);
+       }
+    }//GEN-LAST:event_jComboBox1FocusLost
 
     /**
      * @param args the command line arguments
@@ -106,8 +184,16 @@ public class Pagamento extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton BtnContinuar;
+    private javax.swing.JTextField codigoSegurancaCartao;
     private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JLabel jLTitulo;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField nomeTitular;
+    private javax.swing.JTextField numeroCartao;
+    private javax.swing.JTextField validadeCartao;
     // End of variables declaration//GEN-END:variables
 }
